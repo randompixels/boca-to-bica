88d40cfa1cbad864b971e842fb14e53711ca2d92
Andrei Zahariade <hello@andreiweb.co.uk>
2016-01-17 18:59:42 +0200

    Boca 2 bica

--- Source/content_script.js
+++ Source/content_script.js
@@ -31,10 +31,29 @@ function handleText(textNode)
 {
 	var v = textNode.nodeValue;
 
-	v = v.replace(/\bThe Cloud\b/g, "My Butt");
-	v = v.replace(/\bThe cloud\b/g, "My butt");
-	v = v.replace(/\bthe Cloud\b/g, "my Butt");
-	v = v.replace(/\bthe cloud\b/g, "my butt");
+	v = v.replace(/\bArsenie Boca\b/g, "Marius Bica");
+	v = v.replace(/\bArsenie boca\b/g, "Marius Bica");
+	v = v.replace(/\barsenie Boca\b/g, "marius Bica");
+	v = v.replace(/\barsenie boca\b/g, "marius bica");
+	v = v.replace(/\bArsenie\b/g, "Marius");
+	v = v.replace(/\barsenie\b/g, "marius");
+	v = v.replace(/\bPrislop\b/g, "Innobyte");
+	v = v.replace(/\bprislop\b/g, "innobyte");
+
+	v = v.replace(/\bmormantul\b/g, "biroul");
+	v = v.replace(/\bmormant\b/g, "birou");
+	v = v.replace(/\bmormântul\b/g, "biroul");
+	v = v.replace(/\bmormânt\b/g, "birou");
+	v = v.replace(/\bMormantul\b/g, "Biroul");
+	v = v.replace(/\bMormant\b/g, "Birou");
+	v = v.replace(/\bMormântul\b/g, "Biroul");
+	v = v.replace(/\bMormânt\b/g, "Birou");
+
+	v = v.replace(/\bcredincios\b/g, "programator");
+	v = v.replace(/\bCredincios\b/g, "Programator");
+	v = v.replace(/\bcredincioși\b/g, "programatori");
+	v = v.replace(/\bCredincioși\b/g, "Programatori");
+
 	
 	textNode.nodeValue = v;
 }
